04aebfa3e432aad1d16310b96717b845b0b3ee41
daucen <daucen.osborn@west-mec.org>
2016-04-29 14:55:27 -0700

    This is the functional header page and all of the files necessary for minimalize stuff.

---
+++ header.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <title>Parallax Template - Materialize</title>
+    <link rel="icon" type="image/png" href="moviez.png">
+
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+    <!--Javascript-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/materialize.js"></script>
+    <script type="text/javascript" src="scripts.js"></script>
+</head>
+
+<script type="text/javascript">
+    $('.button-collapse').sideNav({menuWidth: 240, activationWidth: 70, edge: 'right'});
+</script>
+
+<style>
+    .brand-logo {
+        width: 1.7em;
+        height: 1.7em;
+        margin-left: 1.5em;
+        margin-top: .1em;
+    }
+
+         /* Style The Dropdown Button */
+     .dropbtn {
+         color: white;
+         font-size: 16px;
+         border: none;
+         cursor: pointer;
+         height: 4em;
+     }
+
+    /* The container <div> - needed to position the dropdown content */
+    .dropdown {
+        position: relative;
+        display: inline-block;
+        height: 2em;
+    }
+
+    /* Dropdown Content (Hidden by Default) */
+    .dropdown-content2 {
+        display: none;
+        position: relative;
+        background-color: #f9f9f9;
+        min-width: 160px;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    }
+
+    /* Links inside the dropdown */
+    .dropdown-content2 a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
+
+    /* Change color of dropdown links on hover */
+    .dropdown-content2 a:hover {
+        background-color: #f1f1f1
+    }
+
+    /* Show the dropdown menu on hover */
+    .dropdown:hover .dropdown-content2 {
+        display: block;
+    }
+
+    /* Change the background color of the dropdown button when the dropdown content is shown */
+    .dropdown:hover .dropbtn {
+        background-color: #d50000;
+    }
+</style>
+
+<!-- Dropdown Structure -->
+<ul id="dropdown1" class="dropdown-content">
+    <li><a href="#!">one</a></li>
+    <li><a href="#!">two</a></li>
+    <li class="divider"></li>
+    <li><a href="#!">three</a></li>
+</ul>
+<nav class="red darken-4">
+    <div class="nav-wrapper">
+        <a href="index.php" class="brand-logo"><img class="brand-logo" src="moviez.png"></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="login.php">Login</a></li>
+            <li><a href="signup.php">Sign Up</a></li>
+            <!-- Dropdown Trigger -->
+            <li><div class="dropdown">
+                    <button class="dropbtn red darken-4">Movie Categories</button>
+                    <div class="dropdown-content2">
+                        <a href="#">Action</a>
+                        <a href="#">Anime</a>
+                        <a href="#">Comedy</a>
+                        <a href="#">Documentary</a>
+                        <a href="#">Drama</a>
+                        <a href="#">Family</a>
+                        <a href="#">Horror</a>
+                        <a href="#">Sci-Fi</a>
+                    </div>
+                </div></li>
+        </ul>
+    </div>
+</nav>
