a4146e2effd68dd87333ec7b0b1e1164ed681016
daucen <daucen.osborn@west-mec.org>
2016-05-03 15:27:24 -0700

    I made a footer! and attempted to add the php to the createMoviePage.php

--- header.php
+++ header.php
@@ -15,12 +15,13 @@ foreach($dbh->query($query) as $row) {
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-    <title>Parallax Template - Materialize</title>
-    <link rel="icon" type="image/png" href="moviez.png">
+    <title>Moviez</title>
+    <link rel="icon" type="image/png" href="moviez.png"/>
 
     <!-- CSS  -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
     <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="styles.css"/>
 </head>
 
 <style>
