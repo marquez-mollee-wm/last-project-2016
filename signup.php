ebe906b982ff07974e1e3aba8e76875c9bbc59cc
jacob <jacob.riggs@west-mec.org>
2016-05-04 15:13:16 -0700

    Finished the log in and sign up pages and they function fully, on my side at least.

--- signup.php
+++ signup.php
@@ -1,75 +1,65 @@
 <html>
 <head>
-    <title>Signup for Moviez</title>
+    <title>Signup for MovieZ</title>
     <link rel="stylesheet" href="signstyle.css">
 </head>
 <body>
-<br>
+<body>
+
 <header>
+    </br>
     <a href="index.html" class="logo">
-        <strong><em>Moviez</em></strong>
+        <em><strong>MovieZ</strong></em>
     </a>
-    <nav>
-        <ul>
-        </ul>
-    </nav>
 </header>
 <section>
     <h1>Welcome to <em>Moviez</em></h1>
-    <p>Make an account to be able to make a page </p>
+    <p>Make an account to be able to make a page.</p>
 </section>
 <?php
-require_once ('appvars.php');
-$error_msg = "";
-define('MZ_UPLOADPATH', 'images/');
-$dbh = new PDO('mysql:host=localhost;dbname=MovieZ', 'root', 'root');
-if(isset($_POST['submit'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $email = $_POST['email'];
-    $profilePic = $_FILES['profilePic']['name'];
-    $profilePic_size = $_FILES['profilePic']['size'];
-    $profilePic_type = $_FILES['profilePic']['type'];
+require_once('appvars.php');
 
-    if (!empty($username) && !empty($password) && !empty($email) && !empty($profilePic)) {
-        if ((($profilePic_type == 'image/gif') || ($profilePic_type == 'image/jpeg') ||
-                ($profilePic_type == 'image/pjpeg') || ($profilePic_type == 'image/png')) &&
-            ($profilePic_size > 0) && ($profilePic_size <= MZ_MAXFILESIZE)
-        ) {
-            if ($_FILES['file']['error'] == 0)
-                $target = MZ_UPLOADPATH . $profilePic;
-            if (move_uploaded_file($_FILES['profilePic']['tmp_name'], $target)) {
-                $name = $_POST['name'];
-                $score = $_POST['score'];
-                if (!empty($name) && !empty($score)) {
-                    $query = "INSERT INTO users (username, password, email, profilePic) VALUES (:username, :password, :email, :picture) ";
-                    $stmt = $dbh->prepare($query);
-                    $result = $stmt->execute(
-                        array(
-                            'username' => $username,
-                            'password' => $password,
-                            'email' => $email,
-                            'profilePic' => $profilePic,
-                        )
-                    );
-                    echo '<p>Thanks for creating an account!</p>';
-                    echo '<p><a href="index.html">&lt;&lt; Back to Main Page</a></p>';
-                    // Clear the score data to clear the form
-                    $username = "";
-                    $passsword = "";
-                    $email = "";
-                } else {
-                    echo '<p class="error">The screen shot must be a GIF, JPEG, or PNG image file no ' .
-                        'greater than ' . (MZ_MAXFILESIZE / 1024) . ' KB in size.</p>';
-                    @unlink($_FILES['profilePic']['tmp_name']);
-                }
-            } else {
-                $error_msg = "Please enter all fields";
-            }
+// Connect to the database
+$dbh = new PDO('mysql:host=localhost;dbname=MovieZ', 'root', 'root' );
+
+if (isset($_POST['submit'])) {
+    // Grab the profile data from the POST
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+    $email = trim($_POST['email']);
+
+    if (!empty($username) && !empty($password) && !empty($email)) {
+        // Make sure someone isn't already registered using this username
+        $query = "SELECT * FROM users WHERE username = '$username'";
+        $stmt = $dbh->prepare($query);
+        $stmt->execute(
+            array(
+                'username'  => $username
+            )
+        );
+
+        $results = $stmt->fetchAll();
+        if (count($results) == 0) {
+            // The username is unique, so insert the data into the database
+            $query = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
+            $data = $dbh->prepare($query);
+            $data->execute(array('username'=> $username, 'password'=> $password));
+
+            // Confirm success with the user
+            echo '<p>Your new account has been successfully created. You\'re now ready to <a href="login.php">log in</a>.</p>';
+
+            exit();
+        }
+        else {
+            // An account already exists for this username, so display an error message
+            echo '<p class="error">An account already exists for this username. Please use a different address.</p>';
+            $username = "";
         }
     }
+    else {
+        echo '<p class="error">You must enter all of the sign-up data, including the desired password twice.</p>';
+    }
 }
-
 
 ?>
 <div class="form-wrap">
@@ -80,16 +70,15 @@ if(isset($_POST['submit'])) {
 
     <div class="tabs-content">
         <div id="signup-tab-content" class="active">
-            <form class="signup-form" action="" method="post">
-                <input type="text"  class="input" id="username" name="username" autocomplete="on" placeholder="Username">
-                <input type="password" class="input" id="password" name="username" autocomplete="off" placeholder="Password">
-                <input type="email" class="input" id="email" name="email" autocomplete="off" placeholder="Email">
-                <input type="file" class="input" id="screenshot" name="screenshot">
-                <input type="submit" name="submit" class="button" value="Sign Up">
+            <form class="signup-form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                 <input type="text" class="input" id="username" name="username" autocomplete="on" placeholder="Username" value="<?php if (!empty($username)) echo $username; ?>" /><br />
+                 <input type="password" class="input" id="password" name="password" autocomplete="off" placeholder="Password" /><br />
+                 <input type="email" class="input" id="email" name="email" autocomplete="off" placeholder="Email" /><br />
+                 <input type="submit" value="Sign Up" class="button" name="submit" />
             </form>
         </div>
-
     </div>
 </div>
 </body>
 </html>
+
