c1db822de67ba8134900783ca336d12b0733f778
athina bitsui <athina.bitsui@west-mec.org>
2016-05-06 14:49:19 -0700

    the index is done..

--- index.php
+++ index.php
@@ -7,14 +7,14 @@ require_once ('header.php');
 <style>
     #pic1{
         width: 35em;
-        left: 4em;
+        left: 6em;
         margin-top: 5em;
     }
     #howItWorks{
         width: 90em;
         right: 20em;
-        margin-top: -20em;
-        margin-bottom: 8em;
+        margin-top: -30em;
+        margin-bottom: 15em;
     }
 </style>
 
@@ -26,11 +26,11 @@ require_once ('header.php');
             <img class="activator" src="moviez.png">
         </div>
         <div class="card-content">
-            <span class="card-title activator grey-text text-darken-4">About<i class="material-icons right">more_vert</i></span>
+            <span class="card-title activator red-text text-red-4">About<i class="material-icons right">more_vert</i></span>
         </div>
         <div class="card-reveal">
-            <span class="card-title grey-text text-darken-4">About<i class="material-icons right">close</i></span>
-            <p>Moviez it is a place where you go to put reviews or a page about the movie they have watch. To say how good or bad the movie is. Or other people would read upon or the rating of the movie before the they actually watch the movie.The web has a variety of categories to romance to war. And this page is cridick free, this all based off of all the fans</p>
+            <span class="card-title red-text text-red-4">About<i class="material-icons right">close</i></span>
+            <p class="red-text">Welcome to Moviez. Moviez it is a place where you go to put reviews or a page about the movie they have watch. To say how good or bad the movie is. Or other people would read upon or the rating of the movie before the they actually watch the movie.The web has a variety of categories to Action to Sci-fi. And this page is critic free, this all based off of all the fans</p>
         </div>
     </div>
 </div>
@@ -39,7 +39,7 @@ require_once ('header.php');
     <div class="col s12 m5 right">
         <div class="card-panel red">
             <h3 class="white-text">How It Works</h3>
-          <span class="white-text">This is how the moviez works. After you are done with the movie you have watch, you can go make a page of the movie then put it a category, than in a rating (G or PG) of movie, and how good it is. This is all depending on you how good it is.
+          <span class="white-text">This is how the moviez works. After you are done with the movie you have watch, you can go make a page of the Moviez then put it a category, than in a rating (G or R) of movie, and how good it is. This is all depending on you how good it is.
           </span>
         </div>
     </div>
@@ -48,6 +48,5 @@ require_once ('header.php');
 <?php
 require_once ('footer.php');
 ?>
-
 
 </body>
